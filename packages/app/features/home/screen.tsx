facdc58b87fe8861075809551d37690f7955ce58
Big Boss <bigboss@metalrodeo.xyz>
2023-08-26 12:06:44 -0500

    repro

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,15 +1,17 @@
 import {
+  Adapt,
   Anchor,
   Button,
   H1,
   Paragraph,
+  Popover,
   Separator,
   Sheet,
   useToastController,
   XStack,
   YStack,
 } from '@my/ui'
-import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
+import { ChevronDown, ChevronUp, Menu } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
 
@@ -17,6 +19,7 @@ export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
   })
+  const [open, setOpen] = useState(false)
 
   return (
     <YStack f={1} jc="center" ai="center" p="$4" space>
@@ -48,7 +51,63 @@ export function HomeScreen() {
       <XStack>
         <Button {...linkProps}>Link to user</Button>
       </XStack>
+      <Popover open={open} onOpenChange={setOpen} size="$5" stayInFrame={{ padding: 20 }}>
+        <Popover.Trigger asChild>
+          <Button
+            chromeless
+            p="$2"
+            onPress={() => setOpen(!open)}
+            theme={open ? 'alt1' : ''}
+            icon={<Menu size={32} />}
+          />
+        </Popover.Trigger>
 
+        <Adapt platform="web" when="sm">
+          <Popover.Sheet zIndex={100000000} modal dismissOnSnapToBottom>
+            <Popover.Sheet.Frame>
+              <Popover.Sheet.ScrollView>
+                <Adapt.Contents />
+              </Popover.Sheet.ScrollView>
+            </Popover.Sheet.Frame>
+            <Popover.Sheet.Overlay zIndex={100} />
+          </Popover.Sheet>
+        </Adapt>
+
+        <Popover.Content
+          bw={1}
+          boc="$borderColor"
+          enterStyle={{ x: 0, y: -10, o: 0 }}
+          exitStyle={{ x: 0, y: -10, o: 0 }}
+          x={0}
+          y={0}
+          o={1}
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          p={0}
+          maxHeight="80vh"
+          elevate
+          zIndex={100000000}
+        >
+          <Popover.Arrow borderWidth={1} boc="$borderColor" />
+
+          <Popover.ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
+            <YStack
+              miw={230}
+              p="$3"
+              ai="flex-end"
+              // display={open ? 'flex' : 'none'}
+            >
+              <Paragraph>Nothing to see here</Paragraph>
+            </YStack>
+          </Popover.ScrollView>
+        </Popover.Content>
+      </Popover>
       <SheetDemo />
     </YStack>
   )
@@ -94,4 +153,11 @@ function SheetDemo() {
       </Sheet>
     </>
   )
+}
+function useNextRouter() {
+  throw new Error('Function not implemented.')
+}
+
+function useEffect(arg0: () => () => void, arg1: any[]) {
+  throw new Error('Function not implemented.')
 }
